ca919873febec9e8c8b608427d9a1261f7c3495d
Jasem Duncan <jasemvalencia@gmail.com>
2022-12-10 22:56:43 -0500

    add Bill class and Item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,31 +1,29 @@
 class Item ()
-    attr_reader :params
+  attr_reader :params
 
-    def initialize(params)    
-        @quantity=params[:quantity]    
-        @name = params[:name]
-        @price = params[:price]
-        @type = params[:type]
-        @imported = params[:imported]
+  def initialize(params)    
+    @quantity=params[:quantity]    
+    @name = params[:name]
+    @price = params[:price]
+    @type = params[:type]
+    @imported = params[:imported]
+  end
 
+  def calculateTax
+    tax =0.0;
+    if ["book","food","medical"].include?(@type)
+      if @imported == false
+        tax=0.0;                
+      else
+        tax=5;
+      end                    
+    else
+      if @imported == false
+        tax = 10;
+      else
+        tax = 15;
+      end
     end
-
-    def calculateTax
-        tax =0.0;
-        if ["book","food","medical"].include?(@type)
-            if @imported == false
-                # puts "im ehere";
-                tax=0.0;                
-            else
-                tax=5;
-            end                    
-        else
-            if @imported == false
-                tax = 10;
-            else
-                tax = 15;
-            end
-        end
-        return (((@quantity*@price*tax)/100)*20).round.to_f/20
-    end    
+    return (((@quantity * @price * tax)/100) * 20).round.to_f/20
+  end    
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,82 +1,79 @@
-require  './lib/item'
-require  './lib/bill'
+require  './lib/Item'
 
 #Bill 1
 describe "Exempted 10% because is book" do
-    describe "Exempted 5% because is not imported" do 
-        it "Should be tax 0" do
-            input = {
-                    quantity: 2,
-                    name: "book",
-                    price: 12.49,
-                    type: "book",
-                    imported: false
-                }
-
-            expect(Item.new(input).calculateTax()).to eql(0.0)
-        end
+  describe "Exempted 5% because is not imported" do 
+    it "Should be tax 0" do
+      input = {
+            quantity: 2,
+            name: "book",
+            price: 12.49,
+            type: "book",
+            imported: false
+      }
+      expect(Item.new(input).calculateTax()).to eql(0.0)
     end
+  end
 end 
 
 describe "Not exempted 10% because is not book, food or medical" do
-    describe " Exemped 5% because is not imported" do 
-        it "Should not be tax 0" do
-            input = {
-                quantity: 1,
-                name: "music CD",
-                price: 14.99,
-                type: "music",
-                imported: false
-            }
-
-        expect(Item.new(input).calculateTax()).to eql(1.5)
-        end
+  describe " Exemped 5% because is not imported" do 
+    it "Should not be tax 0" do
+      input = {
+        quantity: 1,
+        name: "music CD",
+        price: 14.99,
+        type: "music",
+        imported: false
+      }
+      expect(Item.new(input).calculateTax()).to eql(1.5)
     end
+  end
 end 
 
 describe "Exempted 10% because is food" do
-    describe "Exempted 5% because is not imported" do 
-        it "Should be tax 0" do
-            input = {
-                quantity: 1,
-                name: "chocolate",
-                price: 0.85,
-                type: "food",
-                imported: false
-            }
-        expect(Item.new(input).calculateTax()).to eql(0.0)
-        end
+  describe "Exempted 5% because is not imported" do 
+    it "Should be tax 0" do
+      input = {
+        quantity: 1,
+        name: "chocolate",
+        price: 0.85,
+        type: "food",
+        imported: false
+      }
+      expect(Item.new(input).calculateTax()).to eql(0.0)
     end
+  end
 end 
 
 #Bill 2
 describe "Exempted 10% because is food" do
-    describe "Not exemped 5% because is imported" do 
-        it "Should not be tax 0" do
-            input = {
-                quantity: 1,
-                name: "Imported box of chocolate",
-                price: 10.00,
-                type: "food",
-                imported: true
-            }
-            expect(Item.new(input).calculateTax()).to eql(0.5)
-        end
+  describe "Not exemped 5% because is imported" do 
+    it "Should not be tax 0" do
+      input = {
+        quantity: 1,
+        name: "Imported box of chocolate",
+        price: 10.00,
+        type: "food",
+        imported: true
+      }
+      expect(Item.new(input).calculateTax()).to eql(0.5)
     end
+  end
 end 
 
 describe "Not exempted 10% because is not book, food or medical" do
-    describe "Not exemped 5% because is imported" do 
-        it "Should not be tax 0" do
-            input = {
-                quantity: 1,
-                name: "imported bottle of perfume",
-                price: 47.50,
-                type: "perfum",
-                imported: true
-            }
-        expect(Item.new(input).calculateTax()).to eql(7.15)
-        end
+  describe "Not exemped 5% because is imported" do 
+    it "Should not be tax 0" do
+      input = {
+        quantity: 1,
+        name: "imported bottle of perfume",
+        price: 47.50,
+        type: "perfum",
+        imported: true
+      }
+      expect(Item.new(input).calculateTax()).to eql(7.15)
     end
+  end
 end 
 
